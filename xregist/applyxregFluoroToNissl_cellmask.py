9a03b455b3a830151ce2347113b806b37f0c64af
Bingxing Huo <bingxing.huo@gmail.com>
2018-02-01 09:48:00 -0500

    fixed JP2 registration issue

--- xregist/applyxregFluoroToNissl_cellmask.py
+++ xregist/applyxregFluoroToNissl_cellmask.py
@@ -1,20 +1,19 @@
 import numpy as np
 import os, math, sys
 import SimpleITK as sitk
-#import cv2
+import cv2
 
 sys.path.insert(0,"/home/bingxing/scripts/Connectivity_matrix/xregist")
 #sys.path.insert(0,"/Users/bingxinghuo/Documents/GITHUB/Connectivity_matrix/xregist")
 import ndreg2D
 
 def main():
-    template = sitk.ReadImage(sys.argv[1], sitk.sitkFloat32)# full resolution grayscale image to be deformed
-    width=sys.argv[2]
-    height=sys.argv[3]
-#    target=cv2.imread(sys.argv[2]) # full resolution target image
-#    target=sitk.GetImageFromArray(target,isVector=True)
-#    transformfile = open(sys.argv[3])
-    transformfile = open(sys.argv[4])
+    target=cv2.imread(sys.argv[1]) # full resolution target image
+    target=sitk.GetImageFromArray(target,isVector=True)
+    template=cv2.imread(sys.argv[2]) # full resolution image to be deformed
+    template2D=sitk.GetImageFromArray(template,isVector=True)
+    template2D.SetSpacing(target.GetSpacing())
+    transformfile = open(sys.argv[3])
     # read the transformation matrix
     with transformfile as f:
         euler2d=f.read().splitlines()
@@ -22,8 +21,7 @@ def main():
     euler2d=map(float,euler2d)
 
 # apply transformation
-#    outImg = ndreg2D.imgApplyAffine2D(template,euler2d,size=target.GetSize())
-    outImg = ndreg2D.imgApplyAffine2D(template,euler2d,size=[width,height])
+    outImg = ndreg2D.imgApplyAffine2D(template,euler2d,size=target.GetSize())
 
     sitk.WriteImage(outImg,sys.argv[4])
     transformfile.close()
